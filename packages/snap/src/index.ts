c81cf07a21adc8f696065bb479e531c4c4e45c33
Frederik Bolding <frederik.bolding@gmail.com>
2024-01-05 17:18:28 +0100

    Add repro

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,36 +1,18 @@
 import type { OnRpcRequestHandler } from '@metamask/snaps-sdk';
-import { panel, text } from '@metamask/snaps-sdk';
+import { createPublicClient, http } from 'viem';
+import { mainnet } from 'viem/chains';
 
-/**
- * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
- *
- * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
- * @param args.request - A validated JSON-RPC request object.
- * @returns The result of `snap_dialog`.
- * @throws If the request method is not valid for this snap.
- */
-export const onRpcRequest: OnRpcRequestHandler = async ({
-  origin,
-  request,
-}) => {
-  switch (request.method) {
-    case 'hello':
-      return snap.request({
-        method: 'snap_dialog',
-        params: {
-          type: 'confirmation',
-          content: panel([
-            text(`Hello, **${origin}**!`),
-            text('This custom confirmation is just for display purposes.'),
-            text(
-              'But you can edit the snap source code to make it do something, if you want to!',
-            ),
-          ]),
-        },
-      });
-    default:
-      throw new Error('Method not found.');
-  }
+export const onRpcRequest: OnRpcRequestHandler = async () => {
+  // 1. Import modules.
+
+  // 2. Set up your client with desired chain & transport.
+  const client = createPublicClient({
+    chain: mainnet,
+    transport: http(),
+  });
+
+  // 3. Consume an action!
+  const blockNumber = await client.getBlockNumber();
+
+  return blockNumber;
 };
